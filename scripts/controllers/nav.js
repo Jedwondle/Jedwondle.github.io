0dde044b9252b2b9b65afe54ea1e9dda0350b272
Jonathan Law <jedwondle@gmail.com>
2014-06-09 14:13:08 -0600

    Added title stuff

--- scripts/controllers/nav.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-app.controller('NavCtrl', ['$scope', '$location', function ($scope, $location) { /*jshint unused: false*/
-
-  /*******************************************************************************
-  * This Controller gives us a place to add functionality to the navbar
-  *******************************************************************************/
-
-}]);
