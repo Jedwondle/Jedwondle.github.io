7f7b29ead4f0a51dbc5abf4d565fdb1d53474c91
Jonathan Law <jedwondle@gmail.com>
2014-06-16 13:41:17 -0600

    Updated the website to the current dist

--- scripts/common/animations.js
+++ scripts/common/animations.js
@@ -289,7 +289,7 @@ var openFilter = function (filters, results, details, paginationDiv, windowWidth
   setPageMargin(details, -offset);
   if (openClick === 1) {
     results.stop(true, true).animate({
-      'margin-left': '250px',
+      'margin-left': '250px'
     }, 200 , function() {});
     paginationDiv.stop(true, true).animate({
       'margin-right':'-250px'
